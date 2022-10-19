7d9dbb47ce00157ef6d8c7a0377d84fbffe6f481
davit hermansyah <davit@logique.co.id>
2022-10-19 22:06:18 +0700

    method

--- Customer.php
+++ Customer.php
@@ -3,6 +3,26 @@
 enum Gender: string {
     case Male = "Mr.";
     case Female = "Mrs.";
+
+    static function fromIndonesia(string $value): Gender {
+        return match($value) {
+            "Tuan" => Gender::Male,
+            "Nyonya" => Gender::Female,
+            default => throw new Exception("Unsuported Indonesian Value")
+        };
+    }
+
+    function sayHello() {
+        return "Hello ".$this->value;
+    }
+
+    function inIndonesia(): string 
+    {
+        return match($this) {
+            Gender::Male => "Tuan",
+            Gender::Female => "Nyonya"
+        };
+    }
 }
 
 class Customer {
@@ -27,3 +47,10 @@ echo sayHello(new Customer("2", "Sinta", Gender::Female)).PHP_EOL;
 echo sayHello(new Customer("3", "Sinta", Gender::tryFrom("Salah"))).PHP_EOL;
 
 var_dump(Gender::cases());
+
+var_dump(Gender::Male->sayHello());
+var_dump(Gender::Female->sayHello());
+var_dump(Gender::Female->inIndonesia());
+var_dump(Gender::fromIndonesia("Tuan"));
+var_dump(Gender::fromIndonesia("Nyonya"));
+// var_dump(Gender::fromIndonesia("salah")); //error

f1a7f4dd64e3b0316338fa5a06d9275335cc446c
davit hermansyah <davit@logique.co.id>
2022-10-19 22:18:50 +0700

    promot property

--- Category.php
+++ Category.php
@@ -1,21 +1,15 @@
 <?php
 
 class Category {
-    public readonly string $id;
-    public readonly string $name;
 
-    // public function __construct()
-
-    public function __construct(string $id, string $name) {
-        $this->id = $id;
-        $this->name = $name;
+    public function __construct(public readonly string $id, public readonly string $name) {
     }
 }
 
 $category = new Category("1", "Gedget");
 
 var_dump($category->id);
-$category->id = "123";
+// $category->id = "123";
 
 // $category->setId("1");
 // $category->setId("2");

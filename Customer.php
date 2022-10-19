4eea4ed1667ffcd5a0d5821fedf0f177d3e032e3
davit hermansyah <davit@logique.co.id>
2022-10-19 21:58:38 +0700

    backer enum

--- Customer.php
+++ Customer.php
@@ -1,28 +1,29 @@
 <?php
 
-enum Gender {
-    case Male;
-    case Female;
+enum Gender: string {
+    case Male = "Mr.";
+    case Female = "Mrs.";
 }
 
 class Customer {
 
-    public function __construct(public string $id, public string $name, public Gender $gender) 
+    public function __construct(public string $id, public string $name, public ?Gender $gender) 
     {
     }
 }
 
 function sayHello(Customer $customer): string {
-    if($customer->gender == Gender::Male) {
-        return "Hello Mr. ".$customer->name;
-    }elseif($customer->gender == Gender::Female) {
-        return "Hello Mrs. ".$customer->name;
-    }else {
+
+    if($customer->gender == null){
         return "Hello ".$customer->name;
+    }else {
+        return "Hello ".$customer->gender->value.$customer->name;
     }
+
 }
 
-echo sayHello(new Customer("1", "Budi", Gender::Male));
-echo sayHello(new Customer("2", "Sinta", Gender::Female));
+echo sayHello(new Customer("1", "Budi", Gender::from("Mr."))).PHP_EOL;
+echo sayHello(new Customer("2", "Sinta", Gender::Female)).PHP_EOL;
+echo sayHello(new Customer("3", "Sinta", Gender::tryFrom("Salah"))).PHP_EOL;
 
 var_dump(Gender::cases());
